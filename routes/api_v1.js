6c534cfe879fe7543aa40e84e7fcf1213e6f0477
Fredwin Rosales <frosales@novopayment.com>
2018-01-04 00:30:19 -0400

    Crear usuario en mongodb

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -3,9 +3,21 @@ const router = express.Router();
 
 const User = require('../models/user');
 
-router.get('/users', (req, res)=>{
-	User.find({}, (err, users)=>{
+router.get('/users', (req, res) => {
+	User.find({}, (err, users) => {
 		res.json(users);
+	});
+});
+
+router.post('/users', (req, res) => {
+	delete req.body._id;
+	User.create(req.body, (err, user) => {
+		if(err) {
+			res.json(err);
+		}
+		else {
+			res.json(user);
+		}
 	});
 });
 
