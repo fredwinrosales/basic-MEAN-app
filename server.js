7e45b2f38e3c23cc6460375674dc04b5d05d9a3f
Fredwin Rosales <frosales@novopayment.com>
2018-01-03 23:53:11 -0400

    Conectar con mongodb
    Se crea archivo de rutas

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const path = require('path');
+
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const apiRouter = require('./routes/api_v1');
+
 const app = express();
+mongoose.connect('mongodb://localhost/meanstack_db');
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+// router
+app.use('/', apiRouter);
 
 // static files
-app.use(express.static(__dirname + '/public/dist'));
+app.use(express.static(path.join(__dirname, '/public/dist')));
 
 app.listen(3000, ()=>{
 	console.log('server on port 3000');
